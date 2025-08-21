f4157a39f23c6b2f6d822cb62b3564b96f7b6266
fatimabdulr <fatimahh00m@gmail.com>
2025-08-21 15:20:50 +0300

    Add files via upload

---
+++ color_recognition.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+
+# افتح الفيديو (غيري الاسم لو عندك فيديو ثاني)
+cap = cv2.VideoCapture("855474-hd_1920_1080_24fps.mp4")
+
+# تحديد نطاق اللون الأحمر بالـ HSV
+lower_red = np.array([0, 120, 70])
+upper_red = np.array([10, 255, 255])
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break  # يوقف إذا خلص الفيديو
+
+    # تحويل من BGR إلى HSV
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # نعمل Mask للون الأحمر
+    mask = cv2.inRange(hsv, lower_red, upper_red)
+
+    # نطلع الأجزاء اللي فيها اللون الأحمر
+    result = cv2.bitwise_and(frame, frame, mask=mask)
+
+    # نعرض الفريم الأصلي + الناتج
+    cv2.imshow("Original", frame)
+    cv2.imshow("Red Detection", result)
+
+    # إذا ضغطت q يوقف
+    if cv2.waitKey(25) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
